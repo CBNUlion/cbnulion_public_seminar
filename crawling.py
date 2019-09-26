aa227fdcfbec4384fab0f165c39204cb66ba6858
jinmay <britzegs@gmail.com>
2019-09-26 12:25:49 +0900

    Finished

--- crawling.py
+++ crawling.py
@@ -3,23 +3,21 @@ import requests
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
 
-url = 'https://datalab.naver.com/keyword/realtimeList.naver?where=main'
-headers = {
-    'User-Agent': UserAgent().chrome
-}
+url = "https://datalab.naver.com/keyword/realtimeList.naver?where=main"
+headers = {"User-Agent": UserAgent().chrome}
 response = requests.get(url, headers=headers)
 
 # 요청한 페이지 확인하기
 # print(response.content)
 
-html = bs(response.text.encode("utf-8"), 'lxml')
-cols = html.select('.rank_list.v2')
-titles = html.select('.rank_title.v2')
+html = bs(response.text.encode("utf-8"), "lxml")
+cols = html.select(".rank_list.v2")
+titles = html.select(".rank_title.v2")
 
-header_list = ['순위', '검색어']
+header_list = ["순위", "검색어"]
 for col, title in zip(cols, titles):
-    with open('result/{}.csv'.format(title.get_text()), 'wt', newline='') as f:
+    with open("{}.csv".format(title.get_text()), "wt", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(header_list)
-        for idx, v in enumerate(col.find_all('span', {'class': 'title'}), 1):
+        for idx, v in enumerate(col.find_all("span", {"class": "title"}), 1):
             writer.writerow([idx, v.get_text()])
